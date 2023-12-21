7c928a88e71eaeaf8fe7a4a59f2120c971f6e0c2
motogp-bit <78108539+motogp-bit@users.noreply.github.com>
2023-12-21 16:50:25 +0200

    changes

--- #ASK 2.py
+++ #ASK 2.py
@@ -1,5 +1,7 @@
 #ASK 2
 import numpy as np
+import math
+import time
 import random
 error = 0.00001
 def f(x):
@@ -8,63 +10,70 @@ def df(x):
     return 324*(x**5) + 225*(x**4) - 408*(x**3) - 207*(x**2) + 70*x + 16
 def ddf(x):
     return (df(x+error) - df(x)) / error
-def new(x0):
+def new(x0,count):
     if abs(f(x0)) - error > 0:
-        return new(x0 - 1/((df(x0)/f(x0))-(ddf(x0)/(2*df(x0)))))
+        count+=1
+        return new(x0 - 1/((df(x0)/f(x0))-(ddf(x0)/(2*df(x0)))),count)
     else:
+        #print(count)
         return round(x0,5)
 i = -2
 set1 = set({})
-#function has at most 6 real solutions,so we split our search into 6 equal subsets of length (2-(-2))/6 = 4/6 = 2/3
-#this way we have better odds of finding the solutions
 while (i < 2):
-    set1.add(new(i))
+    set1.add(new(i,0))
     i+=2/3
-for i in set1:
-    print(i)
-
+#for i in set1:
+ #   print(i)
+#print("Newton over")
 def bis(a,b):
-    c = (b-a) * random.random() + a
+    count = 0
+    c = (b-a) * abs(math.sin(time.time())) + a
     while abs(f(c)) - error > 0 and a!=b:
         if (f(c) < 0):
             b = c
         else:
             a = c     
-        c = (b-a) * random.random() + a
+        c = (b-a) * abs(math.sin(time.time())) + a
+        count+=1
+    #print(count)
     return round(c,5)            
 set2 = set({})
 i = -2
-while i+1<2:
-    set2.add(bis(i,i+1))
-    i+=1
-for i in set2:
-    print(i)   
+while i+2/3<2:
+    set2.add(bis(i,i+2/3))
+    i+=2/3
+#for i in set2:
+    #print(i)   
 
+a = -2 
+b = -1
 #running bisection 20 times on [-2,-1] to compare convergence
-for j in range(20):
+
+c = (b-a) * random.random() + a
+counts = 0
+while abs(f(c)) - error > 0 and a!=b:
+    if (f(c) < 0):
+        b = c
+    else:
+        a = c     
     c = (b-a) * random.random() + a
-    counts = 0
-    while abs(f(c)) - error > 0 and a!=b:
-        if (f(c) < 0):
-            b = c
-        else:
-            a = c     
-        c = (b-a) * random.random() + a
-        counts+=1
-    print(counts)
-    
-def sec(a,b,c):
+    counts+=1
+#print(counts)
+#print("Secant method")
+def sec(a,b,c,count):
     if abs(f(c)) - error > 0:
         r = f(c) / f(b)
         q = f(a) / f(b)
         s = f(c) / f(a)
-        return sec(b,c,c - (r*(r-q)*(c-b) + (1-r)*s*(c-a))/((q-1)*(r-1)*(s-1)))
+        count+=1
+        return sec(b,c,c - (r*(r-q)*(c-b) + (1-r)*s*(c-a))/((q-1)*(r-1)*(s-1)),count)
     else:
+        #print(count)
         return round(c,5)
 set3 = set({})
 i = -2
 while (i+2/3<2):
-    set3.add(sec(i,i+1/3,i+2/3))
+    set3.add(sec(i,i+1/3,i+2/3,0))
     i+=(1/3)
-for i in set3:
-    print(i)  
+#for i in set3:
+ #   print(i)  
