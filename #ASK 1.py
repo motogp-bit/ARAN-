1cecec548a775ca0e74823e0f9360413aed674d8
motogp-bit <78108539+motogp-bit@users.noreply.github.com>
2023-12-21 14:14:53 +0200

    Update #ASK 1.py

--- #ASK 1.py
+++ #ASK 1.py
@@ -1,6 +1,6 @@
 #ASK 1
-import math
 import numpy as np
+import math
 error = 0.00001
 def f(x):
     return 12 - 26*x +20*(x**2)-7*(x**3) -12*(math.exp(x-2)) +14*x*(math.exp(x-2))
@@ -20,34 +20,32 @@ def bis(a,b):
         else:
             a = c
         c = (a+b)/2.0
-    print("Number of iterations:",count)
+    #print("Number of iterations:",count)
     return round(c,5)
-print("Bisection method on both roots.")
 #f(0) > 0,f(1) < 0,f(3) > 0
-print(bis(0,1))
-print(bis(1,3))
+#print(bis(0,1))
+#print(bis(1,3))
 def nnew(x0,count):
     if abs(f(x0)) - error > 0:
         count+=1
         return nnew(x0 - (f(x0)/numdiff(f(x0))),count)
     else:
-        print("Number of iterations:",count)
+        #print("Number of iterations:",count)
         return round(x0,5)
-print("Newton method on with initial guesses 1 and 2 respectively,numerical derivative.")
-print(nnew(1,0))
-print(nnew(2,0))
+#print("Newton method on with initial guesses 1 and 2 respectively,numerical derivative.")
+#print(nnew(1,0))
+#print(nnew(2,0))
 def dnew(x0,count):
     if abs(f(x0)) - error > 0:
         count+=1
         return dnew(x0 - (f(x0)/exdiff(f(x0))),count)
     else:
-        print("Number of iterations:",count)
+        #print("Number of iterations:",count)
         return round(x0,5)
-print("Newton method with initial guesses 1 and 2 respectively,exact derivative.")
-print(dnew(1,0))
-print(dnew(2,0))
+#print("Newton method with initial guesses 1 and 2 respectively,exact derivative.")
+#print(dnew(1,0))
+#print(dnew(2,0))
 
-#Η μεθοδος newton συγκλινει τετραγωνικα μονο για ριζες με πολλαπλοτητα 1
 def sec(x0,x1,count):
     if abs(f(x1)) - error > 0:
         count+=1
@@ -55,6 +53,6 @@ def sec(x0,x1,count):
     else:
         print("Number of iterations:",count)
         return round(x1,5)
-print("Secant method with initial guesses (0,1),(2,3) respectively.")
-print(sec(1,0,0))
-print(sec(3,2,0))
+#print("Secant method with initial guesses (0,1),(2,3) respectively.")
+#print(sec(1,0,0))
+#print(sec(3,2,0))
