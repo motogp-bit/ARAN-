b1c3fd4aaf169778460e314a4af21ac89164659b
motogp-bit <78108539+motogp-bit@users.noreply.github.com>
2023-12-22 19:04:20 +0200

    ask3

--- #ASK 3.py
+++ #ASK 3.py
@@ -1,8 +1,4 @@
 import numpy as np
-array = [[1,2,3],[-1,3,4],[0,2,-2]]
-vector = [1,1,2]
-#works for any nxn array
-#in theory,PermMatrix is not needed,I could just swap the rows of the initial array directly
 def PALU(array,vector):
     def upptsub(arr,vector):
         length = len(arr)
@@ -16,7 +12,6 @@ def PALU(array,vector):
             returnedVector[i] = (vector[i] - sum) / arr[i][i] 
         return returnedVector
     
-
     def lowtsub(arr,vector):
         length = len(array)
         returnedVector = [0 for i in range(length)]
@@ -29,28 +24,35 @@ def PALU(array,vector):
             returnedVector[i] = (vector[i] - sum) / array[i][i] 
         return returnedVector
 
-    length = length(array)
-    permMatrix = [[0] for i in range(length)]
-    L = [[0] for i in range(length)]
+    length = len(array)
+    L = []
     for i in range(length):
-        L[i][i] = 1
+        temp = []
+        for j in range(length):
+            if i == j:
+                temp.append(1)
+            else:
+                temp.append(0)
+        L.append(temp)
     max = array[0][0]
     row = 0
-    for i in range (length):
+    for i in range(length):
         if (array[i][0] > max):
             row = i
             max = array[i][0]
-    row = 0
+    permMatrix = []
     for i in range(length):
-        for j in range(length):               
+        temp = []
+        for j in range(length):     
             if i == j:
-                permMatrix[i][j] = 1
+                temp.append(1)
             else:
-                permMatrix[i][j] = 0
-        permMatrix[row][row] = 0
-        permMatrix[0][0] = 0
-        permMatrix[0][row] = 1
-        permMatrix[row][0] = 1
+                temp.append(0)
+        permMatrix.append(temp)
+    permMatrix[row][row] = 0
+    permMatrix[0][0] = 0
+    permMatrix[0][row] = 1
+    permMatrix[row][0] = 1
     array = np.matmul(permMatrix,array)
     z = 0
     for i in range(z+1,length):
@@ -61,45 +63,51 @@ def PALU(array,vector):
 
     return upptsub(array,lowtsub(L,vector))
 
+A = [[4,-1,1],[-1,5,2],[1,2,6]]
+
 def cholesky(a):
     length = len(a)
-    L = [[0] for i in range(length)]
+    L = [[0 for i in range(length)] for j in range(length)]
     L[0][0] = np.sqrt(a[0][0])
     for i in range(1,length):
         L[i][0] = a[i][0] / L[0][0]
 
     for i in range(1,length):
-        for j in range(1,length):
-            if j > i:
-                continue
-            elif j == i:
-                sum = 0
-                for z in range(i):
-                    sum+=L[i][z] * L[i][z]
+        for j in range(i+1):
+            sum = 0
+            if j == i:
+                for z in range(j):
+                    sum+=L[j][z] * L[j][z]
                 L[i][i] = np.sqrt(a[i][i] - sum)
             else:
-                sum = 0
-                for z in range(i):
+                for z in range(j):
                     sum +=L[i][z] * L[j][z]
-                L[j][i] = (a[j][i] - sum)/L[i][i]
+                if (L[j][j] > 0):
+                    L[i][j] = (a[j][i] - sum)/L[j][j]
     return L
 
 def gauss(n):
-    A = [[0] for i in range(n)]
+    A = []
+    k = 0
     for i in range(n):
-        A[i][i] = 5
-        if i < n-1:
-            A[i+1][i] = -2
-            A[i][i+1] = -2
+        temp = [0 for k in range(n)]
+        for j in range(n):
+            if j == i:
+                temp[j] = 5
+                if i != n-1:
+                    temp[j+1] = -2
+                if i!= 0:
+                    temp[j-1] = -2
+            else:
+                temp[j] = 0
+        A.append(temp)
     b = []
-    b[0] = 3
-    b[n-1] = 3
-    for i in range(1,n-1):
-        b[i] = 1
+    b.append(3)
+    for i in range(n-2):
+        b.append(1)
+    b.append(3)
     x = [0 for i in range(n)]
-    exact = np.linalg.solve(A,b)
-    r = [exact[i] - x[i] for i in range(n)]
-    while np.linalg.norm(r,np.inf) > 0.0001:
+    def iterate(x):
         x1 = x
         for i in range(n):
             sum1 = 0
@@ -109,9 +117,12 @@ def gauss(n):
             for j in range(i+1,n):
                 sum2 = sum2 + A[i][j]*x[j]
             x1[i] = (1/A[i][i])*(b[i] - sum1 - sum2)
+        return x1
+
+    while(True):
+        x1 = iterate(x)
+        r = [x1[i] - x[i] for i in range(n)]
+        if np.linalg.norm(r,np.inf) < 0.0001:
+            return x
         x = x1
-        r = [exact[i] - x[i] for i in range(n)]
-    return x
-print(gauss[10])
-print(gauss[10000])
 
